212726c9743bd473d077242d001657e82f3c5d66
Nicolas Chan <nicolas@nicolaschan.com>
2017-08-22 20:25:31 -0700

    Made test work

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
-
 var assert = require('assert');
 // use assert.doesNotThrow and assert.ok
 
 // Unit tests for DataStructures.js, to make sure helper functions were done properly
 /* */
-var DS = require("../DataStructures.js");
-var cs61a = new CustomClass("CS61A");
+var DS = require("../js/DataStructures");
+var cs61a = new DS.CustomClass("CS61A");
 
 /* */
