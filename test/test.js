115f631c2674da652ad79e39c3aa8a08b61b1b56
Nicolas Chan <nicolas@nicolaschan.com>
2017-10-21 14:39:55 -0700

    Build

--- test/test.js
+++ test/test.js
@@ -5,6 +5,8 @@ describe('BellTimer', function() {
     const BellTimer = require('../src/BellTimer');
 
     describe('#constructor', function() {
-
+        it('test arithmetic works', function() {
+            assert(2 + 2 == 4);
+        });
     });
 });
