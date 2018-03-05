13291c04f90a71ebb2b86c1753830e791dfe025b
Nicolas Chan <nicolas@nicolaschan.com>
2018-03-05 00:10:36 -0800

    Revert "Remove unused code"
    This reverts commit d3d9fb805a197ed263bb76c8c57ab5def27d3fa4.

---
+++ webpack.production.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [new UglifyJsPlugin({
+    cache: true
+  })]
+})
