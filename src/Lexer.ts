390761de24093f9f0ada00c67d15065403cc4bf3
Nicolas Chan <nicolas@nicolaschan.com>
2018-08-07 02:23:36 -0700

    Make lexer use typescript

--- src/Lexer.ts
+++ src/Lexer.ts
@@ -1,6 +1,6 @@
-const separators = ['*', ' ', '{', '}', '#']
+const separators: string[] = ['*', ' ', '{', '}', '#']
 
-function splitAndPreserve (str, chars) {
+function splitAndPreserve (str: string, chars: string[]): string[] {
   var pieces = ['']
   for (let c of str) {
     if (chars.indexOf(c) > -1) {
@@ -13,30 +13,30 @@ function splitAndPreserve (str, chars) {
   return pieces.filter(str => str.length > 0)
 }
 
-function lex (line) {
+function lex (line: string): string[] {
   return splitAndPreserve(line, separators)
 }
 
-function drop (char, arr) {
+function drop (char: string, arr: string[]): string[] {
   while (arr[0] === char) { arr = arr.slice(1) }
   return arr
 }
 
-function dropEnd (char, arr) {
+function dropEnd (char: string, arr: string[]): string[] {
   while (arr[arr.length - 1] === char) { arr = arr.slice(0, -1) }
   return arr
 }
 
-function trim (char, arr) {
+function trim (char: string, arr: string[]): string[] {
   return drop(char, dropEnd(char, arr))
 }
 
-function concat (arr) {
+function concat (arr: string[]): string {
   return arr.reduce((a, b) => a.concat(b), '')
 }
 
-function remove (char, str) {
+function remove (char: string, str: string): string {
   return concat(str.split(char))
 }
 
-module.exports = { splitAndPreserve, lex, drop, dropEnd, trim, concat, remove }
+export { splitAndPreserve, lex, drop, dropEnd, trim, concat, remove }
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,23 @@
-require('webpack')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   entry: {
-    stats: ['babel-polyfill', './src/stats.js'],
-    enter: ['babel-polyfill', './src/enter.js'],
-    classes: ['babel-polyfill', './src/classes.js'],
-    settings: ['babel-polyfill', './src/settings.coffee'],
-    periods: ['babel-polyfill', './src/periods.coffee'],
-    online: ['babel-polyfill', './src/online.js'],
-    'client-mithril': ['babel-polyfill', './src/client-mithril.js'],
-    'service-worker': ['babel-polyfill', './src/service-worker.js']
-  },
-  resolve: {
-    alias: {}
-  },
-  output: {
-    path: path.join(__dirname, 'bin'),
-    filename: '[name].js'
+    'client-mithril': ['babel-polyfill', './src/client-mithril.js']
   },
   module: {
-    rules: [{
-      test: /\.coffee$/,
-      use: ['coffee-loader']
-    }, {
-      test: /\.js$|\.jsx$/,
-      use: {
-        loader: 'istanbul-instrumenter-loader',
-        options: {
-          esModules: true
-        }
-      },
-      enforce: 'post',
-      exclude: /node_modules|\.spec\.js$/
-    }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
-  plugins: [new UglifyJsPlugin({
-    cache: true
-  })]
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
