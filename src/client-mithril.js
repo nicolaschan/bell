a4afacbcbb3d76f96d9ae77d6f662d97270c2e11
Nicolas Chan <nicolas@nicolaschan.com>
2018-08-14 23:52:40 -0700

    Auto-refresh when version updated

--- src/client-mithril.js
+++ src/client-mithril.js
@@ -19,6 +19,24 @@ setInterval(function () {
   mithrilUI.redraw()
 }, 1000 / 10)
 
+// TODO: Might be a good idea to move this somewhere else
+const requestManager = require('./RequestManager2').default
+let version
+const checkForNewVersion = async () => {
+  try {
+    const newVersion = await requestManager.get('/api/version')
+    if (version === undefined) {
+      version = newVersion
+    } else if (version !== newVersion) {
+      window.location.reload()
+    }
+  } catch (e) {
+    // no big deal, try again later
+  }
+}
+setInterval(checkForNewVersion, 4 * 60 * 1000)
+checkForNewVersion()
+
 $(window).on('load', async function () {
   logger.info(`bell-countdown version ${VERSION}`)
 
