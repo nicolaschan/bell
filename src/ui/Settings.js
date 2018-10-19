22cf9606240bf278100a638e69e79580d7b75f22
Nicolas Chan <nicolas@nicolaschan.com>
2018-10-19 12:26:04 -0700

    Use default theme for settings if no cookie

--- src/ui/Settings.js
+++ src/ui/Settings.js
@@ -48,7 +48,7 @@ const Settings = {
         }),
         m('.desc', 'Theme'),
         m(Select, {
-          value: cookieManager.get('theme', 'Default - Light'),
+          value: new ThemeManager(cookieManager.get('theme')).currentThemeName,
           options: themeManager.availableThemes,
           onselect: theme => cookieManager.set('theme', theme)
         }),
