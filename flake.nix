7cee31d967e3a5aee0dab38d119396c7b70c5966
Nicolas Chan <nicolas@nicolaschan.com>
2025-11-26 17:15:12 -0800

    Upgrade to node 24

--- flake.nix
+++ flake.nix
@@ -6,15 +6,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_22
+            nodejs_24
             yarn
             python3
             firefox
